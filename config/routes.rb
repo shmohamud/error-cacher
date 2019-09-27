236373c5bfe46a37e91cf00615db39afac8ad168
shmohamud <shmohamud@email.wm.edu>
2019-09-26 22:06:00 -0400

    first

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'jobs/index'
+  get 'jobs/show'
+  get 'jobs/create'
+  get 'jobs/update'
+  get 'jobs/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
